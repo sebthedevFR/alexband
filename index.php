ed3933560f8c61855e4d35df8e910048ddb3aa37
Sebastien <s.toursel@gmail.com>
2016-04-27 15:08:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Inconcessus Lux Lucis</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/stylesheet.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Metal+Mania' rel='stylesheet' type='text/css'>
+  <link rel="icon" type="image/ico" href="images/favicon.ico">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand title" href="#"><p>Inconcessus Lux Lucis</p></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="https://inconcessusluxlucis.bandcamp.com/music">MUSIC</a></li>
+        <li><a href="https://inconcessusluxlucis.bandcamp.com/merch">MERCH</a></li>
+        <li><a href="https://www.facebook.com/inconcessusluxlucis358/">TOUR</a></li>
+        <li><a href="https://www.facebook.com/inconcessusluxlucis358/">MEDIA</a></li>
+          <ul class="dropdown-menu">
+            <li><a href="#">Merchandise</a></li>
+            <li><a href="#">Extras</a></li>
+            <li><a href="#">Media</a></li> 
+          </ul>
+        </li>
+        <li><a href="#"><i class="fa fa-bomb"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+<div class="container text-center">
+
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active">
+	      <img src="images/main5.jpg" alt="New York">
+	      <div class="carousel-caption">
+	        <h3></h3>
+	        <p></p>
+	      </div> 
+	    </div>
+	  </div>
+  </div>
+
+
+  <h2>Inconcessus Lux Lucis</h2>
+  <p><em>Saturnian Black Magick</em></p>
+  <p>I, Voidhanger Records</p>
+  <p></p>
+	<div class="row">
+	  <div class="col-sm-4 bw pic">
+	    <p class="text-center"><strong>A. Baal</strong></p><br>
+	    <a href="#demo" data-toggle="collapse">
+	      <img src="images/alex.jpg" class="img-circle person" alt="Random Name">
+	    </a>
+	    <div id="demo" class="collapse">
+	      <p>Bass Player</p>
+	      <p>Loves long walks on the beach</p>
+	      <p>Member since 1988</p>
+	    </div>
+	  </div>
+
+	  <div class="col-sm-4">
+	    <p class="text-center"><strong></strong></p><br>
+	      <img src="images/sigil.png" class="img-circle" alt="Random Name">
+	  </div>
+
+	  <div class="col-sm-4 bw pic">
+	    <p class="text-center"><strong>W. Malphas</strong></p><br>
+	    <a href="#demo3" data-toggle="collapse">
+	      <img src="images/will.jpg" class="img-circle person" alt="Random Name">
+	    </a>
+	    <div id="demo3" class="collapse">
+	      <p>Guitarist and Lead Vocals</p>
+	      <p>Loves math</p>
+	      <p>Member since 2005</p>
+	    </div>
+	  </div>
+	</div>
+
+
+
+        <div class="row">
+
+            <div class="col-lg-12">
+                <h1 class=""></h1>
+            </div>
+
+            <div id="wrapper" class="col-lg-4 col-md-4 col-xs-6 thumb bw pic">
+                <a class="thumbnail thumbnailimage" href="https://i-voidhangerrecords.bandcamp.com/album/crux-lupus-corona" target="_blank">
+                    <img class="img-responsive" src="images/thumb1.jpg" alt="">
+                </a>
+                <h3>Crux Lupus Corona</h3>
+            </div>
+            <div class="col-lg-4 col-md-4 col-xs-6 thumb bw pic">
+                <a class="thumbnail thumbnailimage" href="https://inconcessusluxlucis.bandcamp.com/album/disintegration-psalms-of-veneration-for-the-nefarious-elite" target="_blank">
+                    <img class="img-responsive" src="images/thumb2.jpg" alt="">
+                </a>
+                <h5>Disintegration: Psalms Of Veneration For The Nefarious Elite</h5>
+            </div>
+            <div class="col-lg-4 col-md-4 col-xs-6 thumb bw pic">
+                <a class="thumbnail thumbnailimage" href="https://inconcessusluxlucis.bandcamp.com/album/severed-from-sephiroth" target="_blank">
+                    <img class="img-responsive" src="images/thumb3.jpg" alt="">
+                </a>
+                <h2>Severed From Sephiroth</h2>
+            </div>
+        </div>
+        </div>
+
+
+
+
+
+
+
+<footer class="text-center">
+<br><br>
+<div id="socialicons">
+<a class="icon" href="LINK" title="Follow with Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+<a class="icon" href="LINK" title="Follow with YouTube" target="_blank"><i class="fa fa-youtube"></i></a>
+<a class="icon" href="LINK" title="Follow with RSS" target="_blank"><i class="fa fa-soundcloud"></i></a>
+<a class="icon" href="mailto:s.toursel@gmail.com" title="Email Me"><i class="fa fa-envelope"></i></a>
+</div>
+<p>Made by <a href="http://sebastientoursel.com/">Sebastien Toursel<a></p>
+</footer>
+
+<script>
+$(document).ready(function(){
+    // Initialize Tooltip
+    $('[data-toggle="tooltip"]').tooltip(); 
+})
+</script>
+
+<script>
+
+</script>
+
+
+
+</body>
+</html>
---
+++ media.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Media | Inconcessus Lux Lucis</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/stylesheet.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Metal+Mania' rel='stylesheet' type='text/css'>
+  <link rel="icon" type="image/ico" href="images/favicon.ico">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand title" href="index.php"><p>Inconcessus Lux Lucis</p></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="https://inconcessusluxlucis.bandcamp.com/music">MUSIC</a></li>
+        <li><a href="https://inconcessusluxlucis.bandcamp.com/merch">MERCH</a></li>
+        <li><a href="https://www.facebook.com/inconcessusluxlucis358/">TOUR</a></li>
+        <li><a href="https://www.facebook.com/inconcessusluxlucis358/">MEDIA</a></li>
+          <ul class="dropdown-menu">
+            <li><a href="#">Merchandise</a></li>
+            <li><a href="#">Extras</a></li>
+            <li><a href="#">Media</a></li> 
+          </ul>
+        </li>
+        <li><a href="#"><i class="fa fa-bomb"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
+
+
+   <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+                <h1 class="page-header">Media</h1>
+            </div>
+
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail " href="#">
+                    <img class="img-responsive " src="images/live3.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#">
+                    <img class="img-responsive" src="images/live2.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#">
+                    <img class="img-responsive thumbimages" src="images/live4.jpg" alt="">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#">
+                    <img class="img-responsive" src="images/live5.jpg" alt="">
+                </a>
+            </div>
+            
+        </div>
+</div>
+
+
+
+
+
+<footer class="text-center">
+<br><br>
+<div id="socialicons">
+<a class="icon" href="LINK" title="Follow with Facebook" target="_blank"><i class="fa fa-facebook"></i></a>
+<a class="icon" href="LINK" title="Follow with YouTube" target="_blank"><i class="fa fa-youtube"></i></a>
+<a class="icon" href="LINK" title="Follow with RSS" target="_blank"><i class="fa fa-soundcloud"></i></a>
+<a class="icon" href="mailto:s.toursel@gmail.com" title="Email Me"><i class="fa fa-envelope"></i></a>
+</div>
+<p>Made by <a href="http://sebastientoursel.com/">Sebastien Toursel<a></p>
+</footer>
+
+<script>
+$(document).ready(function(){
+    // Initialize Tooltip
+    $('[data-toggle="tooltip"]').tooltip(); 
+})
+</script>
+
+<script>
+
+</script>
+
+
+
+</body>
+</html>
